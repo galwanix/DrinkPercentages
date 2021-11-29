df7135bc9e07f00aea35c9813013573990c5c212
galwanix <galwanix@ACER-OG-CMP>
2021-11-29 18:14:56 +0100

    Logic: zabezpieczenia wersja z rzucaniem wyjątków

--- DrinkPercentages/Logic.cs
+++ DrinkPercentages/Logic.cs
@@ -22,17 +22,44 @@ namespace DrinkPercentages
         public float ContainerSize
         {
             get => containerSize;
-            set => containerSize = value > 0 ? value : 0;
+            set
+            {
+                if (value >= 0)
+                    containerSize = value;
+                else
+                    throw new ArgumentException(
+                        "ContainerSize: Value outside of expected range: ContainerSize = "
+                        + value
+                        + "; Default bounds: X>=0");
+            }
         }
         public float AlcoholPercentage
         {
             get => alcoholPercentage;
-            set => alcoholPercentage = (value > 0 && value < 100) ? value : 0;
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    alcoholPercentage = value;
+                else
+                    throw new ArgumentException(
+                        "AlcoholPercentage: Value outside of expected range: AlcoholPercentage = "
+                        + value
+                        + "; Default bounds: 0<=X<=100");
+            }
         }
         public int ContainerCount
         {
             get => containerNumber;
-            set => containerNumber = value > 0 ? value : 0;
+            set
+            {
+                if (value >= 0)
+                    containerNumber = value;
+                else
+                    throw new ArgumentException(
+                        "ContainerCount: Value outside of expected range: ContainerCount = "
+                        + value
+                        + "; Default bounds: X>=0");
+            }
         }
         public float DrinkVolume => ContainerSize * ContainerCount;
         public float AlcoholVolume => DrinkVolume * AlcoholPercentage / 100;
