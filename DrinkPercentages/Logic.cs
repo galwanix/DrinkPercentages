b748601c409f381b3080896cc5b2c32461cc8d52
galwanix <galwanix@ACER-OG-CMP>
2021-11-29 17:41:57 +0100

    Stworzona minimalna wersja klasy Logic

---
+++ DrinkPercentages/Logic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkPercentages
+{
+    class Logic
+    {
+        private float containerSize,
+            alcoholPercentage;
+        private int containerNumber;
+
+        public Logic()
+        {
+            ContainerCount = 3;
+            ContainerSize = 0.75f;
+            AlcoholPercentage = 3.5f;
+        }
+
+        public float ContainerSize
+        {
+            get => containerSize;
+            set => containerSize = value;
+        }
+        public float AlcoholPercentage
+        {
+            get => alcoholPercentage;
+            set => alcoholPercentage = value;
+        }
+        public int ContainerCount
+        {
+            get => containerNumber;
+            set => containerNumber = value;
+        }
+        public float DrinkVolume => ContainerSize * ContainerCount;
+        public float AlcoholVolume => ContainerSize * ContainerCount * AlcoholPercentage / 100;
+    }
+}
